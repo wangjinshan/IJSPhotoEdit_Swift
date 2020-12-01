0ac6ed2df232d60857a41fc9282690f821692178
山神 <1096452045@qq.com>
2020-12-01 17:52:52 +0800

    pod init

---
+++ ZLPhotoBrowser-master/ClipImageBrowser/ClipImageBrowser/ClipImageBrowser.h
@@ -0,0 +1,18 @@
+//
+//  ClipImageBrowser.h
+//  ClipImageBrowser
+//
+//  Created by 好未来山神 on 2020/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClipImageBrowser.
+FOUNDATION_EXPORT double ClipImageBrowserVersionNumber;
+
+//! Project version string for ClipImageBrowser.
+FOUNDATION_EXPORT const unsigned char ClipImageBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClipImageBrowser/PublicHeader.h>
+
+
